70252ef115064d4d1d68bfcef7ae5ffe17236a84
Ayushman Lakshkar <ayushman.lakshkar7@gmail.com>
2024-01-09 20:33:38 +0530

    K

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Group = require('./models/groupmodel')
 const Message = require('./models/messagemodel')
 
 const dotenv = require('dotenv');
-const cors = require('cors');
+// const cors = require('cors');
 const authroutes = require('./routes/authroutes')
 const contactroutes = require('./routes/contactroutes')
 const chatroutes = require('./routes/chatroutes');
@@ -19,16 +19,16 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 app.use(express.static('public'))
 
 
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
-});
+const io = new Server(server
+    // cors: {
+    //     origin: 'http://localhost:3000',
+    //     methods: ['GET', 'POST']
+    // }
+);
 
 
 io.on("connection", (socket) => {
