1eb0ccc265e2139632f8504c492b12ba921fb053
Ayushman Lakshkar <ayushman.lakshkar7@gmail.com>
2024-01-09 20:39:10 +0530

    K

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 app.use(express.static('public'))
 
 
-const io = new Server(server
-    // cors: {
-    //     origin: 'http://localhost:3000',
-    //     methods: ['GET', 'POST']
-    // }
-);
+const io = new Server(server,{
+    cors: {
+        origin: 'https://ayushmanwhatsappclone.netlify.app',
+        methods: ['GET', 'POST']
+    }
+});
 
 
 io.on("connection", (socket) => {
